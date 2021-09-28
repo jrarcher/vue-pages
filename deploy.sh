68390ba8fc0f674e95e0590a264d15b87e5414e5
Ryan Archer <ryanopolis@gmail.com>
2021-09-27 22:14:24 -0500

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:jrarcher/jrarcher.github.io
+git push -f git@github.com:jrarcher/jrarcher.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:jrarcher/gh-pages.git master
